4efb577c45afbccc54bc319a82791a4661ac2d53
Ricardo <mrtinsricardo@gmail.com>
2022-11-26 18:19:02 +0000

    v3

--- calculator.c
+++ calculator.c
@@ -1,49 +1,57 @@
+/*
+Numero: 2022116161
+Nome: Ricardo Martins
+Curso: EI
+*/
+
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     float num1,num2, result, decimal;
-    int total, i;
-
+    int total, option;
     printf("Enter two numbers: ");
     scanf("%f %f",&num1,&num2);
-
-        do
+    do
+    {
+        puts("\nEnter 1 for + : "); //Ask for an operation
+        puts("Enter 2 for - : ");
+        puts("Enter 3 for * : ");
+        puts("Enter 4 for / : ");
+        puts("Enter 0 to exit\n");
+        scanf("%d",&option);
+        switch(option)
         {
-            printf("\nenter 1 for + : ");
-            printf("\nenter 2 for - : ");
-            printf("\nenter 3 for * : ");
-            printf("\nenter 4 for / : \n");
-            scanf("%d",&i);
-
-            switch(i)
-            {
-            case 1:
-                result = num1+num2;
-                decimal = (ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
-                total+=1;
-                break;
-            case 2:
-                result = num1-num2;
-                decimal = (ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
-                total+=1;
-                break;
-            case 3:
-                result = num1*num2;
-                decimal = (ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
-                total+=1;
-                break;
-            case 4:
-                result = num1/num2;
-                decimal = (ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
-                total+=1;
-                break;
-            default:
-                printf("\nEnter valid option\n");
-            }
+        case 0:
+            break;
+        case 1:
+            result = num1+num2;
+            //floor - rounds downs the nearest integer
+            //ceil - rounds up the nearest integer
+            //if both give the same value its an integer else its a decimal
+            decimal = (ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
+            total+=1; //Increments 1 in the total
+            break;
+        case 2:
+            result = num1-num2;
+            decimal =(ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
+            total+=1;
+            break;
+        case 3:
+            result = num1*num2;
+            decimal =(ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
+            total+=1;
+            break;
+        case 4:
+            result = num1/num2;
+            decimal =(ceil(result) == floor(result)) ? printf("\nAnswer : %.f\n",result) : printf("\nAnswer : %.4f\n",result) ;
+            total+=1;
+            break;
+        default:
+            printf("\nEnter valid option\n");
         }
-        while(i!=0);
-        printf("Number of operations: %d\n",total);
-
-        return (0);
-
+    }
+    while(option!=0);
+    printf("\nNumber of operations: %d\n",total);
+    return (0);
 }
